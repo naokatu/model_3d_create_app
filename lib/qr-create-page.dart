4b0408d876e7fbb5b1097fea60fc74bda3048b7d
naokatu <naokatu4@gmail.com>
2024-08-11 02:08:09 +0900

    update: ファイル名ではなくURLを受け取る

--- lib/qr-create-page.dart
+++ lib/qr-create-page.dart
@@ -1,45 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
-class QrCreatePage extends StatefulWidget {
-  final String filename;
-  const QrCreatePage({super.key, required this.filename});
+class QrCreatePage extends StatelessWidget {
+  final String url;
 
-  @override
-  State<QrCreatePage> createState() => _QrCreatePageState();
-}
-
-class _QrCreatePageState extends State<QrCreatePage> {
-  Future<String> getModelUrl() async {
-    // TODO: モデルのURLをここで作成
-
-    await Future.delayed(Duration(seconds: 2)); // シミュレーションのための遅延
-    return 'https://google.com/';
-  }
+  const QrCreatePage({super.key, required this.url});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<String>(
-      future: getModelUrl(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasData) {
-          return Center(
-              child: Container(
-                color: Colors.white,
-                child: QrImageView(
-                  data: snapshot.data!,
-                  version: QrVersions.auto,
-                  size: 250.0,
-                  backgroundColor: Colors.white,
-                ),
-              )
-          );
-        } else {
-          return const Center(child: Text('No data'));
-        }
-      },
+    return Container(
+      color: Colors.white,
+      child: QrImageView(
+        data: url,
+        version: QrVersions.auto,
+        size: 250.0,
+        backgroundColor: Colors.white,
+      ),
     );
   }
 }
